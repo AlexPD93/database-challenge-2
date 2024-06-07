abc2955f9065afe63efca23b25674f00c2cd875e
Alex <alexperezdavies@gmail.com>
2024-06-07 16:15:01 +0100

    finish workshop

--- src/model/products.js
+++ src/model/products.js
@@ -1,7 +1,14 @@
 const db = require("../database/db.js");
 
 const select_products = db.prepare(/*sql*/ `
-  SELECT id, name, quantity_per_unit, unit_price, units_in_stock, units_on_order FROM products
+  SELECT 
+  id, 
+  name, 
+  quantity_per_unit, 
+  FORMAT('£%.2f', unit_price) AS unit_price, 
+  units_in_stock, 
+  FORMAT('£%.2f', unit_price * units_in_stock) AS stock_value, units_on_order
+  FROM products
 `);
 
 function listProducts() {
@@ -17,8 +24,18 @@ function searchProducts(string) {
   return search_products.all("%" + string + "%");
 }
 
+// What is the  difference between INNER JOIN and JOIN?
+//Why don't we put category.id in the SELECT?
 const get_product = db.prepare(/*sql*/ `
-SELECT id, name FROM products WHERE id LIKE ?
+SELECT 
+products.id, 
+products.name,  
+categories.name AS category_name, 
+categories.description AS category_description 
+FROM products 
+JOIN categories ON 
+products.category_id = categories.id 
+WHERE products.id = ?
 `);
 
 function getProduct(id) {
