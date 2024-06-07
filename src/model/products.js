a011af641625b755f8a6aea028057300f7bcde1d
Alex <alexperezdavies@gmail.com>
2024-06-07 13:30:28 +0100

    complete challenge 3

--- src/model/products.js
+++ src/model/products.js
@@ -1,15 +1,28 @@
 const db = require("../database/db.js");
 
 const select_products = db.prepare(/*sql*/ `
-  SELECT name, quantity_per_unit, unit_price, units_in_stock, units_on_order FROM products
+  SELECT id, name, quantity_per_unit, unit_price, units_in_stock, units_on_order FROM products
 `);
 
 function listProducts() {
   return select_products.all();
 }
 
-// Write and export a new function named searchProducts in model/products. This function should take a search string, then return any products in the products table whose name contains that string. Each product should include the id and name columns. For example searchProducts("iscu") should return [{ id: 19, name: "Teatime Chocolate Biscuits" }].
+const search_products = db.prepare(/*sql*/ `
+SELECT id, name FROM products WHERE name LIKE ?;
+`);
 
-// This function is used in routes/search.js. Once you've finished it you should be able to visit /search in your browser and use the search form to browse the products.
+function searchProducts(string) {
+  //% % used to match any sequence of characters
+  return search_products.all("%" + string + "%");
+}
 
-module.exports = { listProducts };
+const get_product = db.prepare(/*sql*/ `
+SELECT id, name FROM products WHERE id LIKE ?
+`);
+
+function getProduct(id) {
+  return get_product.get(id);
+}
+
+module.exports = { listProducts, searchProducts, getProduct };
